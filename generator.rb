6a45ae0ed22dcf1f6c57d49bdea559256d6d1688
Rich <rich9tolley@gmail.com>
2018-01-10 12:16:29 +0000

    init

---
+++ generator.rb
@@ -0,0 +1,91 @@
+require 'json'
+
+json_file_path = ARGV[0]
+
+def add_default_constructors(useRealm, useMoyaObjectMapper, text_result) 
+	text_result << "  required init?(map: Map) { super.init() }\n\n"
+
+	text_result << "  required init() {\n"
+	text_result << "    super.init()\n"
+	text_result << "  }\n\n"
+
+	text_result << "  required init(value: Any, schema: RLMSchema) {\n"
+	text_result << "    super.init(value: value, schema: schema)\n"
+	text_result << "  }\n\n"
+
+	text_result << "  required init(realm: RLMRealm, schema: RLMObjectSchema) {\n"
+	text_result << "    super.init(realm: realm, schema: schema)\n"
+	text_result << "  }\n"
+
+	text_result
+end
+
+def create_model(name, json) 
+
+	text_result = ""
+	text_result << "import Foundation\n"
+	text_result << "import ObjectMapper\n"
+	text_result << "import Realm\n"
+	text_result << "import RealmSwift\n"
+
+	text_result << "\n"
+	text_result << "class #{name.capitalize}: Object, Mappable {\n"
+
+
+	json.each_pair do |k,v|
+		if v.instance_of? Fixnum 
+			text_result << "  @objc dynamic var #{k}: Int = 0\n"
+		elsif v.instance_of? String 
+			text_result << "  @objc dynamic var #{k}: String = \"\"\n"
+		elsif v.instance_of? Float 
+			text_result << "  @objc dynamic var #{k}: Double = 0.0\n"
+		elsif v.instance_of? Bool 
+			text_result << "  @objc dynamic var #{k}: Bool = false\n"
+		end
+	end
+
+	text_result << "\n"
+
+	text_result = add_default_constructors(true, true, text_result)
+
+	if json.keys.include?("id")
+		text_result << "\n"
+		text_result << "  override class func primaryKey() -> String? {\n"
+		text_result << "    return \"id\"\n"
+		text_result << "  }\n"
+	end
+
+
+	text_result << "\n"
+
+	text_result << "  func mapping(map: Map) {\n"
+
+	json.each_pair { |k,v| text_result << "    #{k} <- map[\"#{k}\"]\n" }
+
+	text_result << "  }\n"
+
+	text_result << "}\n"
+
+	text_result << "\n"
+
+	out_file = File.open("#{name.capitalize}.swift","w")
+
+	out_file.write text_result
+
+	puts text_result
+
+end
+
+
+if json_file_path.length > 0 
+  in_file = File.open(json_file_path,"r")
+  lines = in_file.read
+  json = JSON.parse(lines)
+  
+
+  json.each_pair { |name, val| create_model(name,val) }
+
+end
+
+
+
